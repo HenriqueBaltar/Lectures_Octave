e3c24c4bdfcafd2ede101d42250e84fb2734de6a
Henrique Baltar <82537763+HenriqueBaltar@users.noreply.github.com>
2021-11-08 20:18:53 -0300

    Added Bode diagrams
    Plot Bode diagrams for low-pass RC circuit.

---
+++ BodeDiagram_low-pass_RC.m
@@ -0,0 +1,25 @@
+clear, clc
+
+pkg load control % load when initialize Octave to work with the control pack
+
+R = 1E3;   % 1 kohm
+C = 1E-6;  % 1 microfarad
+tau = R*C  % time constant
+w = 1/tau  % cut-off frequency (rad/s)
+f = w/2/pi % cut-off frequency (Hz)
+
+num = [1];       % numerator
+den = [tau 1];   % denominator
+H = tf(num, den) % transfer function
+
+bode(H)
+
+% another way to do it
+s = tf('s')         % define s
+G = 1/(1+s/w)^2 % then Octave will understand an equation like that
+figure
+bode(G)
+
+K = 1/(1+s/w)/(1+s/(w/1000))
+figure
+bode (K)
